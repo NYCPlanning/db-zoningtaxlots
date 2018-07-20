4fc6f8ae8f93411d22c25d95dceae8acbd623318
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-07-20 11:06:25 -0400

    adding correct file path
    Former-commit-id: b24684cc65cae2fe3f42205ca312ea7088bc8198 [formerly 991f02682fc6d69f6829c75e021129bdb80dc842]
    Former-commit-id: 87c393df32687505d0891b25f40e0f7648b108c7

--- zoningtaxlots_build/02_build.sh
+++ zoningtaxlots_build/02_build.sh
@@ -10,11 +10,11 @@ DBUSER=$(cat $REPOLOC/ztl.config.json | jq -r '.DBUSER')
 
 start=$(date +'%T')
 echo "Starting to build zoning tax lot database"
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/create.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/bbl.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/area_zoningdistrict.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/area_commercialoverlay.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/area_specialdistrict.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/area_limitedheight.sql
-psql -U $DBUSER -d $DBNAME -f $REPOLOC/pluto_build/sql/area_zoningmap.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/create.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/bbl.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/area_zoningdistrict.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/area_commercialoverlay.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/area_specialdistrict.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/area_limitedheight.sql
+psql -U $DBUSER -d $DBNAME -f $REPOLOC/zoningtaxlots_build/sql/area_zoningmap.sql
 echo "Done!"

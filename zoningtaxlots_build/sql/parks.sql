6394d269dc927b93312b24590297b63f000f59ba
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-08-31 15:24:23 -0400

    editing so only 50% rule is applied to COs and modifying so that parks are only nulled out if first ZD is park
    Former-commit-id: dfbd3711ba852788e327f934735c999c7a8e68aa [formerly 5afe3ddb956d4bca641eba2d56d097ac51fdd6a4] [formerly 8b5b8801634e1ae698de4f2bb667307e89a7a5b6 [formerly f386755cebc3e033a761639fb2d8002d3863052e]]
    Former-commit-id: b9fa6607cf16c6814539715d7094a11bf0665ede [formerly 535f8c733488fcf7ffc0cb09bdcb76d18d75c532]
    Former-commit-id: 21bcaa645e5178a3d1cc4cf029343eac1576e456

--- zoningtaxlots_build/sql/parks.sql
+++ zoningtaxlots_build/sql/parks.sql
@@ -4,20 +4,9 @@
 UPDATE dcp_zoning_taxlot a
 SET zoningdistrict1 = 'PARK'
 WHERE zoningdistrict1 = 'BALL FIELD'
-	OR zoningdistrict2 = 'BALL FIELD'
-	OR zoningdistrict3 = 'BALL FIELD'
-	OR zoningdistrict4 = 'BALL FIELD'
 	OR zoningdistrict1 = 'PLAYGROUND'
-	OR zoningdistrict2 = 'PLAYGROUND'
-	OR zoningdistrict3 = 'PLAYGROUND'
-	OR zoningdistrict4 = 'PLAYGROUND'
 	OR zoningdistrict1 = 'PUBLIC PLACE'
-	OR zoningdistrict2 = 'PUBLIC PLACE'
-	OR zoningdistrict3 = 'PUBLIC PLACE'
-	OR zoningdistrict4 = 'PUBLIC PLACE'
-	OR zoningdistrict2 = 'PARK'
-	OR zoningdistrict3 = 'PARK'
-	OR zoningdistrict4 = 'PARK';
+	OR zoningdistrict1 = 'PARK';
 -- where zoningdistrict1 = 'PARK' NULL out all other zoning information
 UPDATE dcp_zoning_taxlot a
 SET zoningdistrict2 = NULL,

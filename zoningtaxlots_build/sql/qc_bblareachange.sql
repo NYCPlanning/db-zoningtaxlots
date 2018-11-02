b7a90d7d16e3f5e2176a7d5065517e05b9dadba2
Amanda Doyle <Amanda.Doyle.GIS@gmail.com>
2018-11-02 16:54:31 -0400

    adding field and qc script to compare area of lot between verisons

---
+++ zoningtaxlots_build/sql/qc_bblareachange.sql
@@ -0,0 +1,9 @@
+-- output a file with bbls that have changed in size between verisons
+CREATE TABLE bblareachange AS (
+SELECT DISTINCT a.bbl, a.area as newarea, b.area as oldarea, (a.area - b.area) as areadiff
+FROM dcp_zoning_taxlot a
+INNER JOIN dcp_zoning_taxlot_prev b
+ON a.bbl=b.bbl);
+
+\copy (SELECT * FROM bblareachange) TO '/prod/db-zoningtaxlots/zoningtaxlots_build/output/qc_bblareachange.csv' DELIMITER ',' CSV HEADER;
+DROP TABLE IF EXISTS bblareachange;

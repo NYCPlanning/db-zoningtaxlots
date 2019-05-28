7466ce90606dfd1578c03e7c458e46e255c263cd
sptkl <caobaiyue@gmail.com>
2019-05-28 16:25:57 -0400

    add zoningmapindex
    Former-commit-id: 57b49a6a04b7174459fcb4499f0d84cf7a98fb9e [formerly b738d94a7b26173485cbeba323e12e5f200312d5] [formerly 7161b0f2d6a415235a9d69cb2631a5f668be7ccd [formerly 5e0c6d7ac171cea2456029bb468b031c8ca2db14]]
    Former-commit-id: 5ee63a98db33d45a1deea3b8ef98ecc53ac4de2d [formerly a9d86ef4e2f8d7840eb8faf941be1be3636ebbdd]
    Former-commit-id: 90815478143d74d3204db9183eb5d37ddb7debf8

---
+++ zoningtaxlots_build/sql/preprocessing.sql
@@ -0,0 +1,36 @@
+-- Preprocessing (change column names and table names) 
+ALTER TABLE dcp_commercialoverlay 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_limitedheight 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_mih 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_specialpurposesubdistricts 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_specialpurpose
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_zoningdistricts 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_zoningtaxlots 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_zoningmapamendments 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_specialpurposesubdistricts 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dof_dtm 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dof_dtm 
+RENAME COLUMN wkb_geometry TO geom;
+
+ALTER TABLE dcp_zoningtaxlots
+RENAME TO dcp_zoning_taxlot;

cc49b012ad8eacdaafd5961436f5bf3d973e577a
sptkl <caobaiyue@gmail.com>
2019-08-19 14:06:02 +0000

    update initialization 01_dataloading.sh
    Former-commit-id: e94f644baf044972d6f8b05d04bd97fbc7c91707 [formerly 769e3cc5233b705bc00aa291953029b94d01dc50] [formerly 5c5621cba3f5210d0672ac6cbb20342ccb0ea7b2 [formerly 0cf9120e9e5a7be47d89b48ef655f84011ed40bb]]
    Former-commit-id: f589dc3155454c7be439c9ab73725ed3829064c3 [formerly 435f7cc2c7fdfd2b505bca7f25b04f89d84eca12]
    Former-commit-id: c9baaa336844f087ee1b5c6ca8350bd7b45b12c0

--- zoningtaxlots_build/01_dataloading.sh
+++ zoningtaxlots_build/01_dataloading.sh
@@ -1,10 +1,26 @@
-sh 01_setup_ztl.sh
-
+# Create a postgres database container ztl
 DB_CONTAINER_NAME=ztl
+
+[ ! "$(docker ps -a | grep $DB_CONTAINER_NAME)" ]\
+     && docker run -itd --name=$DB_CONTAINER_NAME\
+            -v `pwd`:/home/zoningtaxlots_build\
+            -w /home/zoningtaxlots_build\
+            --shm-size=1g\
+            --cpus=2\
+            -p 5435:5432\
+            mdillon/postgis
+
+## Wait for database to get ready, this might take 5 seconds of trys
 docker start $DB_CONTAINER_NAME
+until docker exec $DB_CONTAINER_NAME psql -h localhost -U postgres; do
+    echo "Waiting for postgres container..."
+    sleep 0.5
+done
+
 docker inspect -f '{{.State.Running}}' $DB_CONTAINER_NAME
 docker exec ztl psql -U postgres -h localhost -c "SELECT 'DATABSE IS UP';"
 
+## load data into the ztl container
 docker run --rm\
             --network=host\
             -v `pwd`/python:/home/python\

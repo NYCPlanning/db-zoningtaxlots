041e6e2e40bfd64db4ff6228b1fdc87c80e12966
sptkl <caobaiyue@gmail.com>
2019-12-19 18:03:32 +0000

    update to future

--- zoningtaxlots_build/01_dataloading.sh
+++ zoningtaxlots_build/01_dataloading.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-# Create a postgres database container ztl
-DB_CONTAINER_NAME=ztl
-
-[ ! "$(docker ps -a | grep $DB_CONTAINER_NAME)" ]\
-     && docker run -itd --name=$DB_CONTAINER_NAME\
-            -v `pwd`:/home/zoningtaxlots_build\
-            -w /home/zoningtaxlots_build\
-            --env-file .env\
-            --shm-size=4g\
-            -p 5435:5432\
-            mdillon/postgis
-
-## Wait for database to get ready, this might take 5 seconds of trys
-docker start $DB_CONTAINER_NAME
-until docker exec $DB_CONTAINER_NAME psql -h localhost -U postgres; do
-    echo "Waiting for postgres container..."
-    sleep 0.5
-done
-
-docker inspect -f '{{.State.Running}}' $DB_CONTAINER_NAME
-docker exec ztl psql -U postgres -h localhost -c "SELECT 'DATABSE IS UP';"
-
 ## load data into the ztl container
 docker run --rm\
             --network=host\
--- zoningtaxlots_build/02_build.sh
+++ zoningtaxlots_build/02_build.sh
@@ -1,82 +1,86 @@
 #!/bin/bash
-start=$(date +'%T')
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 echo "Starting to build zoning tax lot database"
-docker exec ztl psql -h localhost -U postgres -f sql/create_priority.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/preprocessing.sql
-docker exec ztl psql -h localhost -U postgres -f sql/archive.sql 
-docker exec ztl psql -h localhost -U postgres -f sql/create.sql 
-docker exec ztl psql -h localhost -U postgres -f sql/bbl.sql
+psql $BUILD_ENGINE -f sql/create_priority.sql &
+psql $BUILD_ENGINE -f sql/preprocessing.sql
+psql $BUILD_ENGINE -f sql/archive.sql 
+psql $BUILD_ENGINE -f sql/create.sql 
+psql $BUILD_ENGINE -f sql/bbl.sql
 
 wait
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict_mn.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict_bk.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict_bx.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict_si.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict_qn.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_commercialoverlay.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_specialdistrict.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_limitedheight.sql &
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningmap.sql & 
-docker exec ztl psql -h localhost -U postgres -f sql/area_mih.sql
+psql $BUILD_ENGINE -f sql/area_zoningdistrict_mn.sql &
+psql $BUILD_ENGINE -f sql/area_zoningdistrict_bk.sql &
+psql $BUILD_ENGINE -f sql/area_zoningdistrict_bx.sql &
+psql $BUILD_ENGINE -f sql/area_zoningdistrict_si.sql &
+psql $BUILD_ENGINE -f sql/area_zoningdistrict_qn.sql &
+psql $BUILD_ENGINE -f sql/area_commercialoverlay.sql &
+psql $BUILD_ENGINE -f sql/area_specialdistrict.sql &
+psql $BUILD_ENGINE -f sql/area_limitedheight.sql &
+psql $BUILD_ENGINE -f sql/area_zoningmap.sql & 
+psql $BUILD_ENGINE -f sql/area_mih.sql
 
 wait
-docker exec ztl psql -h localhost -U postgres -f sql/area_zoningdistrict.sql 
+psql $BUILD_ENGINE -f sql/area_zoningdistrict.sql 
 
 wait
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM zoningmapperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_zoningmapperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"&
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM zoningmapperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_zoningmapperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"&
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM limitedheightperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_limitedheightperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"&
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM limitedheightperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_limitedheightperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"&
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM specialpurposeperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_specialpurposeperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"&
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM specialpurposeperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_specialpurposeperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"&
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM commoverlayperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_commoverlayperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"&
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM commoverlayperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_commoverlayperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"&
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM lotzoneperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_lotzoneperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"&
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM lotzoneperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_lotzoneperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"&
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM mihperorder ORDER BY bbl) 
-                                TO '/home/zoningtaxlots_build/output/intermediate_mihperorder.csv' 
-                                DELIMITER ',' CSV HEADER;"
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM mihperorder ORDER BY bbl) 
+#                                 TO '/home/zoningtaxlots_build/output/intermediate_mihperorder.csv' 
+#                                 DELIMITER ',' CSV HEADER;"
 
 wait
 
-docker exec ztl psql -h localhost -U postgres -f sql/parks.sql
-docker exec ztl psql -h localhost -U postgres -f sql/inzonechange.sql
-docker exec ztl psql -h localhost -U postgres -f sql/correct_duplicatevalues.sql
-docker exec ztl psql -h localhost -U postgres -f sql/correct_zoninggaps.sql
-docker exec ztl psql -h localhost -U postgres -f sql/correct_invalidrecords.sql
+psql $BUILD_ENGINE -f sql/parks.sql
+psql $BUILD_ENGINE -f sql/inzonechange.sql
+psql $BUILD_ENGINE -f sql/correct_duplicatevalues.sql
+psql $BUILD_ENGINE -f sql/correct_zoninggaps.sql
+psql $BUILD_ENGINE -f sql/correct_invalidrecords.sql
 
 echo "export final output"
-docker exec ztl psql -h localhost -U postgres -f sql/export.sql
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT * FROM dcp_zoning_taxlot_export)
-                                TO '/home/zoningtaxlots_build/output/zoningtaxlot_db.csv' 
-                                DELIMITER ',' CSV HEADER;" &
+psql $BUILD_ENGINE -f sql/export.sql
+# psql $BUILD_ENGINE -c "\copy (SELECT * FROM dcp_zoning_taxlot_export)
+#                                 TO '/home/zoningtaxlots_build/output/zoningtaxlot_db.csv' 
+#                                 DELIMITER ',' CSV HEADER;" &
 
-echo "export unique value lookup tables"
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT DISTINCT zonedist FROM dcp_zoningdistricts ORDER BY zonedist) 
-                                TO '/home/zoningtaxlots_build/output/zoningtaxlot_zonedistricts.csv' 
-                                DELIMITER ',' CSV HEADER;" &
+# echo "export unique value lookup tables"
+# psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT zonedist FROM dcp_zoningdistricts ORDER BY zonedist) 
+#                                 TO '/home/zoningtaxlots_build/output/zoningtaxlot_zonedistricts.csv' 
+#                                 DELIMITER ',' CSV HEADER;" &
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT DISTINCT overlay FROM dcp_commercialoverlay ORDER BY overlay) 
-                                TO '/home/zoningtaxlots_build/output/zoningtaxlot_commoverlay.csv' 
-                                DELIMITER ',' CSV HEADER;" &
+# psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT overlay FROM dcp_commercialoverlay ORDER BY overlay) 
+#                                 TO '/home/zoningtaxlots_build/output/zoningtaxlot_commoverlay.csv' 
+#                                 DELIMITER ',' CSV HEADER;" &
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT DISTINCT sdname, sdlbl FROM dcp_specialpurpose ORDER BY sdname) 
-                                TO '/home/zoningtaxlots_build/output/zoningtaxlot_specialdistricts.csv' 
-                                DELIMITER ',' CSV HEADER;" &
+# psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT sdname, sdlbl FROM dcp_specialpurpose ORDER BY sdname) 
+#                                 TO '/home/zoningtaxlots_build/output/zoningtaxlot_specialdistricts.csv' 
+#                                 DELIMITER ',' CSV HEADER;" &
 
-docker exec ztl psql -h localhost -U postgres -c "\copy (SELECT DISTINCT lhname, lhlbl FROM dcp_limitedheight ORDER BY lhname) 
-                                TO '/home/zoningtaxlots_build/output/zoningtaxlot_limitedheight.csv' 
-                                DELIMITER ',' CSV HEADER;"
+# psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT lhname, lhlbl FROM dcp_limitedheight ORDER BY lhname) 
+#                                 TO '/home/zoningtaxlots_build/output/zoningtaxlot_limitedheight.csv' 
+#                                 DELIMITER ',' CSV HEADER;"
 
 wait
 echo "Build is done!"

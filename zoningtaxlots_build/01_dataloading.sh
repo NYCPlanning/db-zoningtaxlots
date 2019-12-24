2fb911459def612dea2a23033aae0dd64abe9328
sptkl <caobaiyue@gmail.com>
2019-12-24 16:32:41 +0000

    fixing dataloading and qaqc

--- zoningtaxlots_build/01_dataloading.sh
+++ zoningtaxlots_build/01_dataloading.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
-## Installing dependencies ...
+#!/bin/sh
+echo "\e[1;34m \nInstalling Python Packages ...\e[0m"
 pip3 install -r requirements.txt
 
-## Dataloading ...
+echo "\e[1;34m \nDataloading ... \e[0m"
 python3 python/future_loading.py

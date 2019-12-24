e5cf0834f274392752a012d1f3ea4623239eba7b
sptkl <caobaiyue@gmail.com>
2019-12-24 16:09:22 +0000

    futurizing

--- zoningtaxlots_build/04_archive.sh
+++ zoningtaxlots_build/04_archive.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
-DATE=$(date "+%Y/%m/%d");
-pg_dump -t dcp_zoning_taxlot --no-owner $BUILD_ENGINE | psql $EDM_DATA
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$DATE\";";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$DATE\";";
+python3 python/archive.py

f4f86c6d2f983b9a1d7556e3dfb36415bec61db7
sptkl <caobaiyue@gmail.com>
2019-12-26 21:51:51 +0000

    remove python dependency

--- zoningtaxlots_build/01_dataloading.sh
+++ zoningtaxlots_build/01_dataloading.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
-## Installing dependencies ...
-pip3 install -r requirements.txt
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
 
-## Dataloading ...
-python3 python/future_loading.py
+schema_names=(
+    'dcp_commercialoverlay'
+    'dcp_limitedheight'
+    'dcp_mih'
+    'dcp_specialpurpose'
+    'dcp_specialpurposesubdistricts'
+    'dcp_zoningtaxlots'
+    'dcp_zoningmapamendments'
+    'dcp_zoningmapindex'
+    'dof_dtm'
+    'dcp_zoningdistricts')
+
+for schema_name in ${schema_names[*]}
+do
+    curl -d "{
+    \"src_engine\":\"${RECIPE_ENGINE}\",
+    \"dst_engine\": \"${BUILD_ENGINE}\",
+    \"src_schema_name\": \"${schema_name}\",
+    \"dst_schema_name\": \"public\",
+    \"src_version\": \"latest\",
+    \"dst_version\": \"${schema_name}\"
+    }"\
+    -H "Content-Type: application/json"\
+    -X POST $GATEWAY/migrate &
+done
--- zoningtaxlots_build/04_archive.sh
+++ zoningtaxlots_build/04_archive.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
-python3 python/archive.py
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
+DATE=$(date "+%Y/%m/%d")
+schema_name='dcp_zoningtaxlots'
+
+curl -d "{
+    \"src_engine\":\"${BUILD_ENGINE}\",
+    \"dst_engine\": \"${EDM_DATA}\",
+    \"src_schema_name\": \"public\",
+    \"dst_schema_name\": \"${schema_name}\",
+    \"src_version\": \"${schema_name}\",
+    \"dst_version\": \"${DATE}\"
+    }"\
+    -H "Content-Type: application/json"\
+    -X POST $GATEWAY/migrate
+
+psql $EDM_DATA -c "DROP VIEW IF EXISTS ${schema_name}.latest"
+psql $EDM_DATA -c "CREATE VIEW ${schema_name}.latest AS 
+                    (SELECT * FROM ${schema_name}.\"${DATE}\")"

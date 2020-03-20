292cd6d9cad9e13e5d921e596b9bbadfa09911dd
sptkl <caobaiyue@gmail.com>
2020-03-20 14:15:02 +0000

    adding export qaqc_frequency and qaqc_bbl to spaces

--- zoningtaxlots_build/04_archive.sh
+++ zoningtaxlots_build/04_archive.sh
@@ -17,4 +17,9 @@ psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$DATE\";";
 psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$DATE\";";
 
 psql $EDM_DATA -v VERSION=$VERSION -f sql/qaqc/frequency.sql
+psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_frequency) 
+    TO STDOUT DELIMITER ',' CSV HEADER;" > output/qaqc_frequency.csv
+
 psql $EDM_DATA -v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV -f sql/qaqc/bbl.sql
+psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_bbl) 
+    TO STDOUT DELIMITER ',' CSV HEADER;" > output/qaqc_bbl.csv

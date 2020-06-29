5c485cbd1dec3fc751187c7c1034b2d9174f3f75
SPTKL <caobaiyue@gmail.com>
2020-06-29 08:54:50 -0400

    [build]
    Former-commit-id: a1422735d954019402417679aafb3030394a20b1 [formerly 80838d88243f75674a40c8528c0dbc2dd526c148] [formerly 7cb70903cc862517ca5f8c521750f827f3e68ded [formerly 04170a561426600bfc66b0a523a2dd21da9bfb8c]]
    Former-commit-id: b423621a050578b73b6092b9077fc04f2caa2601 [formerly 615a79773907bfaa3ca93cc3a466a3c95b100390]
    Former-commit-id: 9a292e642fb72dd3193e323da9a9ad2ca6ae4ecb

--- zoningtaxlots_build/04_archive.sh
+++ zoningtaxlots_build/04_archive.sh
@@ -7,13 +7,15 @@ fi
 pg_dump -t dcp_zoning_taxlot $BUILD_ENGINE | psql $EDM_DATA
 
 DATE=$(date "+%Y/%m/01")
-VERSION=$DATE
-VERSION_PREV=$(date --date="$(date "+%Y/%m/01") - 1 month" "+%Y/%m/01")
+# VERSION=$DATE
+# VERSION_PREV=$(date --date="$(date "+%Y/%m/01") - 1 month" "+%Y/%m/01")
+VERSION='2020/07/01'
+VERSION_PREV='2020/06/01'
 
 psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;";
 psql $EDM_DATA -c "ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$DATE\";";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$DATE\";";
+psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$VERSION\";";
+psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$VERSION\";";
 
 psql $EDM_DATA -v VERSION=$VERSION -f sql/qaqc/frequency.sql
 psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_frequency order by version::timestamp) 

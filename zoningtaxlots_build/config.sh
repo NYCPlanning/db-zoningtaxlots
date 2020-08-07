654c01b609cd6d517e757cf7ab04c48a2c45f674
Molly Graber <molly.graber@me.com>
2020-08-07 19:58:53 -0400

    Refactor QAQC checks to run in EDM (#44)
    * Refactor bbl area change
    
    * Refactor frequency change
    
    * Refactor change in NULLs
    
    * Refactor field changes and switch to temp tables
    
    * qc_bbldiffs (#45)
    
    * adding bbldiffs
    
    * adding running script for bbldiffs
    
    * using config file for bash functions
    
    * refactor runner scripts
    
    * Name qaqc scripts to match output table names
    
    * Fix versions in nownullcount
    
    * Fix ROUND paren in versioncomparison
    
    * Simplify frequencychanges
    
    * Change indentation for legibility
    
    * rewrite CI, making it consistent to other data products (#47)
    
    * rewrite CI, making it consistent to other data products
    
    * refactor main.yml
    
    * refactor qaqc bash script and remove 04 archive
    
    * [build] trigger a build
    
    * [build] selfhosted runners
    
    * [build] remove self-hosted runners
    
    * [build] fix errors in qaqc scripts
    
    * [build]
    
    Co-authored-by: SPTKL <caobaiyue@gmail.com>
    Former-commit-id: 79505057580568125a5096a1e1a7c39aca4da966 [formerly e95b8cae624d7c362fb7355faf8ac9d52dc79836] [formerly aa63869769deb99332cfce8e722fbf0278de7833 [formerly 06df21100dffeb0bf2cbb5d84f637aefb342e413]]
    Former-commit-id: 16daf025f458f067b3bbcb6f84e9512738781b81 [formerly 86ec2e0aed9d7b663f84c67eed67264f7b542aaf]
    Former-commit-id: aefd214a98adf5750952ed98e97d66f3bfd5bbec

---
+++ zoningtaxlots_build/config.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function set_env {
+  for envfile in $@
+  do
+    if [ -f $envfile ]
+      then
+        export $(cat $envfile | sed 's/#.*//g' | xargs)
+      fi
+  done
+}
+
+function urlparse {
+    proto="$(echo $1 | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+    url=$(echo $1 | sed -e s,$proto,,g)
+    userpass="$(echo $url | grep @ | cut -d@ -f1)"
+    BUILD_PWD=`echo $userpass | grep : | cut -d: -f2`
+    BUILD_USER=`echo $userpass | grep : | cut -d: -f1`
+    hostport=$(echo $url | sed -e s,$userpass@,,g | cut -d/ -f1)
+    BUILD_HOST="$(echo $hostport | sed -e 's,:.*,,g')"
+    BUILD_PORT="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+    BUILD_DB="$(echo $url | grep / | cut -d/ -f2-)"
+}
+
+function SHP_export {
+  urlparse $BUILD_ENGINE
+  mkdir -p $@ &&
+    (
+      cd $@
+      ogr2ogr -progress -f "ESRI Shapefile" $@.shp \
+          PG:"host=$BUILD_HOST user=$BUILD_USER port=$BUILD_PORT dbname=$BUILD_DB password=$BUILD_PWD" \
+          -nlt MULTIPOLYGON $@
+        rm -f $@.zip
+        zip $@.zip *
+        ls | grep -v $@.zip | xargs rm
+      )
+}
+
+function Upload {
+  mc rm -r --force spaces/edm-publishing/db-zoningtaxlots/$@
+  mc cp -r output spaces/edm-publishing/db-zoningtaxlots/$@
+}
+
+# Set Environmental variables
+set_env .env version.env
+
+# Parse URL
+urlparse $BUILD_ENGINE
+
+# Set Date
+DATE=$(date "+%Y/%m/01")
+VERSION=$DATE
+VERSION_PREV=$(date --date="$(date "+%Y/%m/01") - 1 month" "+%Y/%m/01")

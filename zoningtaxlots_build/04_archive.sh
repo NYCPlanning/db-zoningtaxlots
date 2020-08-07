c83159363e5bc81d8e192ac127a94e60a61b6362
SPTKL <caobaiyue@gmail.com>
2020-08-07 10:14:24 -0400

    qc_bbldiffs (#45)
    * adding bbldiffs
    
    * adding running script for bbldiffs
    
    * using config file for bash functions
    
    * refactor runner scripts

--- zoningtaxlots_build/04_archive.sh
+++ zoningtaxlots_build/04_archive.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
-
-pg_dump -t dcp_zoning_taxlot $BUILD_ENGINE | psql $EDM_DATA
-
-DATE=$(date "+%Y/%m/01")
-VERSION=$DATE
-VERSION_PREV=$(date --date="$(date "+%Y/%m/01") - 1 month" "+%Y/%m/01")'
-
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$VERSION\";";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$VERSION\";";
+source config.sh
 
 psql $EDM_DATA -v VERSION=$VERSION -f sql/qaqc/frequency.sql
 psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_frequency order by version::timestamp) 
@@ -26,3 +12,6 @@ psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_bbl order by vers
 psql $EDM_DATA -v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV -f sql/qaqc/mismatch.sql
 psql $EDM_DATA -c "\copy (SELECT * FROM dcp_zoningtaxlots.qaqc_mismatch order by version::timestamp) 
     TO STDOUT DELIMITER ',' CSV HEADER;" > output/qaqc_mismatch.csv
+
+psql $EDM_DATA -v VERSION=$VERSION -v VERSION_PREV=$VERSION_PREV -f sql/qaqc/out_bbldiffs.sql |
+psql $BUILD_ENGINE -f sql/qaqc/in_bbldiffs.sql

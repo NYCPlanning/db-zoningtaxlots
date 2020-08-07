9e0b696ee4ea8d730f0c4a997c3246ea65389893
sptkl <caobaiyue@gmail.com>
2020-08-07 15:22:46 +0000

    refactor qaqc bash script and remove 04 archive

--- zoningtaxlots_build/02_build.sh
+++ zoningtaxlots_build/02_build.sh
@@ -16,8 +16,6 @@ psql $BUILD_ENGINE -f sql/area_zoningmap.sql
 
 wait
 psql $BUILD_ENGINE -f sql/area_zoningdistrict.sql 
-
-wait
 psql $BUILD_ENGINE -f sql/parks.sql
 psql $BUILD_ENGINE -f sql/inzonechange.sql
 psql $BUILD_ENGINE -f sql/correct_duplicatevalues.sql
@@ -32,24 +30,10 @@ psql $BUILD_ENGINE -f sql/export.sql
 
 echo "archive final output"
 pg_dump -t dcp_zoning_taxlot $BUILD_ENGINE | psql $EDM_DATA
-psql $EDM_DATA -c "CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;";
-psql $EDM_DATA -c "DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$VERSION\";";
-psql $EDM_DATA -c "ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$VERSION\";";
 
-mkdir -p output && (
-  cd output
-  psql $BUILD_ENGINE -c "\COPY (SELECT * FROM source_data_versions) 
-    TO STDOUT DELIMITER ',' CSV HEADER;" > source_data_versions.csv
-  psql $BUILD_ENGINE -c "\copy (SELECT * FROM dcp_zoning_taxlot_export)
-    TO STDOUT DELIMITER ',' CSV HEADER;" > zoningtaxlot_db.csv
-  echo "export unique value lookup tables"
-  psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT zonedist FROM dcp_zoningdistricts ORDER BY zonedist) 
-    TO STDOUT DELIMITER ',' CSV HEADER;" > zoningtaxlot_zonedistricts.csv
-  psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT overlay FROM dcp_commercialoverlay ORDER BY overlay) 
-    TO STDOUT DELIMITER ',' CSV HEADER;" > zoningtaxlot_commoverlay.csv
-  psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT sdname, sdlbl FROM dcp_specialpurpose ORDER BY sdname) 
-    TO STDOUT DELIMITER ',' CSV HEADER;" > zoningtaxlot_specialdistricts.csv
-  psql $BUILD_ENGINE -c "\copy (SELECT DISTINCT lhname, lhlbl FROM dcp_limitedheight ORDER BY lhname) 
-    TO STDOUT DELIMITER ',' CSV HEADER;" > zoningtaxlot_limitedheight.csv
-)
+psql $EDM_DATA -c "
+  CREATE SCHEMA IF NOT EXISTS dcp_zoningtaxlots;
+  ALTER TABLE dcp_zoning_taxlot SET SCHEMA dcp_zoningtaxlots;
+  DROP TABLE IF EXISTS dcp_zoningtaxlots.\"$VERSION\";
+  ALTER TABLE dcp_zoningtaxlots.dcp_zoning_taxlot RENAME TO \"$VERSION\";
+"

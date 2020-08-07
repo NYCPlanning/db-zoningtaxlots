b2bd107b0133fe9977a7cf62a58b34411a0057db
mgraber <molly.graber@me.com>
2020-08-07 18:27:18 +0000

    Simplify frequencychanges

--- zoningtaxlots_build/sql/qaqc/frequencychanges.sql
+++ zoningtaxlots_build/sql/qaqc/frequencychanges.sql
@@ -1,109 +1,46 @@
 CREATE TEMP TABLE qaqc_frequency_change (
-    WITH newfrequency AS (
-        SELECT 'zoningdistrict1' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningdistrict1 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict2' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningdistrict2 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict3' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningdistrict3 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict4' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningdistrict4 IS NOT NULL
-        UNION
-        SELECT 'commercialoverlay1' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE commercialoverlay1 IS NOT NULL
-        UNION
-        SELECT 'commercialoverlay2' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE commercialoverlay2 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict1' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE specialdistrict1 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict2' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE specialdistrict2 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict3' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE specialdistrict3 IS NOT NULL
-        UNION
-        SELECT 'limitedheightdistrict' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE limitedheightdistrict IS NOT NULL
-        UNION
-        SELECT 'zoningmapnumber' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningmapnumber IS NOT NULL
-        UNION
-        SELECT 'zoningmapcode' as field, COUNT(*) as countnew
-        FROM dcp_zoningtaxlots.:"VERSION" 
-        WHERE zoningmapcode IS NOT NULL),
-    oldfrequency AS (
-        SELECT 'zoningdistrict1' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningdistrict1 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict2' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningdistrict2 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict3' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningdistrict3 IS NOT NULL
-        UNION
-        SELECT 'zoningdistrict4' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningdistrict4 IS NOT NULL
-        UNION
-        SELECT 'commercialoverlay1' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE commercialoverlay1 IS NOT NULL
-        UNION
-        SELECT 'commercialoverlay2' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE commercialoverlay2 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict1' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE specialdistrict1 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict2' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE specialdistrict2 IS NOT NULL
-        UNION
-        SELECT 'specialdistrict3' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE specialdistrict3 IS NOT NULL
-        UNION
-        SELECT 'limitedheightdistrict' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE limitedheightdistrict IS NOT NULL
-        UNION
-        SELECT 'zoningmapnumber' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningmapnumber IS NOT NULL
-        UNION
-        SELECT 'zoningmapcode' as field, COUNT(*) as countold
-        FROM dcp_zoningtaxlots.:"VERSION_PREV"
-        WHERE zoningmapcode IS NOT NULL)
-    SELECT a.field, 
-            a.countnew, 
-            b.countold, 
-            :'VERSION' as version,
-            :'VERSION_PREV' as version_prev
-    FROM newfrequency a
-    JOIN oldfrequency b
-    ON a.field = b.field
-    ORDER BY countnew::numeric - countold::numeric DESC        
+    WITH
+    count_old as (
+        SELECT b.key as field, b.value::numeric as count
+        FROM( SELECT row_to_json(row) as _col 
+        FROM(
+        SELECT 
+            sum(zoningdistrict1 is not null::int) as zoningdistrict1,
+            sum(zoningdistrict2 is not null::int) as zoningdistrict2,
+            sum(zoningdistrict3 is not null::int) as zoningdistrict3,
+            sum(zoningdistrict4 is not null::int) as zoningdistrict4,
+            sum(commercialoverlay1 is not null::int) as commercialoverlay1,
+            sum(commercialoverlay2 is not null::int) as commercialoverlay2,
+            sum(specialdistrict1 is not null::int) as specialdistrict1,
+            sum(specialdistrict2 is not null::int) as specialdistrict2,
+            sum(specialdistrict3 is not null::int) as specialdistrict3,
+            sum(limitedheightdistrict is not null::int) as limitedheightdistrict,
+            sum(zoningmapnumber is not null::int) as zoningmapnumber,
+            sum(zoningmapcode is not null::int) as zoningmapcode
+        FROM dcp_zoningtaxlots.:"VERSION_PREV") row) a, json_each_text(_col) as b
+    ),
+    count_new as (
+        SELECT b.key as field, b.value::numeric as count
+        FROM( SELECT row_to_json(row) as _col 
+        FROM(
+        SELECT 
+            sum(zoningdistrict1 is not null::int) as zoningdistrict1,
+            sum(zoningdistrict2 is not null::int) as zoningdistrict2,
+            sum(zoningdistrict3 is not null::int) as zoningdistrict3,
+            sum(zoningdistrict4 is not null::int) as zoningdistrict4,
+            sum(commercialoverlay1 is not null::int) as commercialoverlay1,
+            sum(commercialoverlay2 is not null::int) as commercialoverlay2,
+            sum(specialdistrict1 is not null::int) as specialdistrict1,
+            sum(specialdistrict2 is not null::int) as specialdistrict2,
+            sum(specialdistrict3 is not null::int) as specialdistrict3,
+            sum(limitedheightdistrict is not null::int) as limitedheightdistrict,
+            sum(zoningmapnumber is not null::int) as zoningmapnumber,
+            sum(zoningmapcode is not null::int) as zoningmapcode
+        FROM dcp_zoningtaxlots.:"VERSION") row) a, json_each_text(_col) as b
+    ) 
+    SELECT a.field, a.count as countold, b.count as countnew
+    FROM count_old a JOIN count_new b on a.field=b.field
+    ORDER BY b.count - a.count DESC       
 );
 
 \COPY qaqc_frequency_change TO PSTDOUT DELIMITER ',' CSV HEADER;

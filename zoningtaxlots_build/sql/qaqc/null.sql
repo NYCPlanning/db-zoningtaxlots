a7b030d1f2851c952e062a4fca3e075b4e79ebd3
sptkl <caobaiyue@gmail.com>
2021-03-25 14:53:46 +0000

    adding qaqc/null.sql
    Former-commit-id: 2f973cb31eb9f1fa0fa60ccf2de26c4d381ab828 [formerly a2caeba4f2e924b49d4505ab7cf4fc1874f69997] [formerly 0d3c3ea318dc4f41439846d200944e89d22eacc5 [formerly 5423b924f7a66edc803655b7b4aea57b31044de1]]
    Former-commit-id: 489589377bf0d16939cf03f69b3aa8365c0f842e [formerly c1d289d7c6967ef2963973ce9b6e94938c670485]
    Former-commit-id: b85d7f113867e9e65d628c6a3bc0e97c5be2be68

--- zoningtaxlots_build/sql/qaqc/null.sql
+++ zoningtaxlots_build/sql/qaqc/null.sql
@@ -72,88 +72,88 @@ CREATE TEMP TABLE qaqc_new_nulls AS (
 			ON a.bbl=b.bbl
 			WHERE a.zoningmapcode IS NULL AND b.zoningmapcode IS NOT NULL
 	),
-	oldnull AS (
+	newvalue AS (
 		SELECT 'zoningdistrict1' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningdistrict1 IS NULL AND b.zoningdistrict1 IS NOT NULL
+			WHERE a.zoningdistrict1 IS NOT NULL AND b.zoningdistrict1 IS NULL
 		UNION
 		SELECT 'zoningdistrict2' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningdistrict2 IS NULL AND b.zoningdistrict2 IS NOT NULL
+			WHERE a.zoningdistrict2 IS NOT NULL AND b.zoningdistrict2 IS NULL
 		UNION
 		SELECT 'zoningdistrict3' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningdistrict3 IS NULL AND b.zoningdistrict3 IS NOT NULL
+			WHERE a.zoningdistrict3 IS NOT NULL AND b.zoningdistrict3 IS NULL
 		UNION
 		SELECT 'zoningdistrict4' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningdistrict4 IS NULL AND b.zoningdistrict4 IS NOT NULL
+			WHERE a.zoningdistrict4 IS NOT NULL AND b.zoningdistrict4 IS NULL
 		UNION
 		SELECT 'commercialoverlay1' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.commercialoverlay1 IS NULL AND b.commercialoverlay1 IS NOT NULL
+			WHERE a.commercialoverlay1 IS NOT NULL AND b.commercialoverlay1 IS NULL
 		UNION
 			SELECT 'commercialoverlay2' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.commercialoverlay2 IS NULL AND b.commercialoverlay2 IS NOT NULL
+			WHERE a.commercialoverlay2 IS NOT NULL AND b.commercialoverlay2 IS NULL
 		UNION
 		SELECT 'specialdistrict1' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.specialdistrict1 IS NULL AND b.specialdistrict1 IS NOT NULL
+			WHERE a.specialdistrict1 IS NOT NULL AND b.specialdistrict1 IS NULL
 		UNION
 		SELECT 'specialdistrict2' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.specialdistrict2 IS NULL AND b.specialdistrict2 IS NOT NULL
+			WHERE a.specialdistrict2 IS NOT NULL AND b.specialdistrict2 IS NULL
 		UNION
 		SELECT 'specialdistrict3' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.specialdistrict3 IS NULL AND b.specialdistrict3 IS NOT NULL
+			WHERE a.specialdistrict3 IS NOT NULL AND b.specialdistrict3 IS NULL
 		UNION
 		SELECT 'limitedheightdistrict' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.limitedheightdistrict IS NULL AND b.limitedheightdistrict IS NOT NULL
+			WHERE a.limitedheightdistrict IS NOT NULL AND b.limitedheightdistrict IS NULL
 		UNION
 		SELECT 'zoningmapnumber' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningmapnumber IS NULL AND b.zoningmapnumber IS NOT NULL
+			WHERE a.zoningmapnumber IS NOT NULL AND b.zoningmapnumber IS NULL
 		UNION
 			SELECT 'zoningmapcode' AS field, COUNT(*)
 			FROM dcp_zoningtaxlots.:"VERSION" a
 			INNER JOIN dcp_zoningtaxlots.:"VERSION_PREV" b
 			ON a.bbl=b.bbl
-			WHERE a.zoningmapcode IS NULL AND b.zoningmapcode IS NOT NULL
+			WHERE a.zoningmapcode IS NOT NULL AND b.zoningmapcode IS NULL
 	)
 	SELECT a.field, 
-			a.count as newnullcount, 
-			b.count as oldnullcount,
+			a.count as value_to_null, 
+			b.count as null_to_value,
 			:'VERSION' as version,
 			:'VERSION_PREV' as version_prev
 	FROM newnull a
-	LEFT JOIN oldnull b
+	LEFT JOIN newvalue b
 	ON a.field=b.field
-	ORDER BY newnullcount, oldnullcount DESC
+	ORDER BY value_to_null, null_to_value DESC
 );
 
 \COPY qaqc_new_nulls TO PSTDOUT DELIMITER ',' CSV HEADER;

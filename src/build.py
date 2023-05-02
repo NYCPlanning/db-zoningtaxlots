cd4c649a3a60e3414f35099fa27655bbc1350fc4
Finn van Krieken <finnvankrieken@gmail.com>
2023-05-02 11:05:18 -0700

    name tasks based on args

--- src/build.py
+++ src/build.py
@@ -2,7 +2,8 @@ from prefect import task, flow
 from utils import run_sql_file
 
 
-@task
+@task(name="Execute sql file",
+      task_run_name="Execute {file}.sql")
 def run_sql_build_file(file):
     run_sql_file("sql", file)
     return True
--- src/dataloading.py
+++ src/dataloading.py
@@ -7,7 +7,8 @@ from constants import input_datasets, sql_engine
 from utils import get_version, get_sql_file, run_sql_file, library_sql_folder
 
 
-@task
+@task(name="Import dataset",
+      task_run_name="Import {dataset}")
 def import_dataset(dataset: str, version: str = "latest"):
     version = get_version(dataset, version=version)
     # Download sql dump for the datasets from data library
@@ -17,7 +18,7 @@ def import_dataset(dataset: str, version: str = "latest"):
     return True
 
 
-@task
+@task(name="Create versions table")
 def create_versions_table():
     with sql_engine.begin() as conn:
         conn.execute(

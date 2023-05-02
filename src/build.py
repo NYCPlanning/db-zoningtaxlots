535b2a38fb367b918bc21327cc207fb47dc2d170
Finn van Krieken <finnvankrieken@gmail.com>
2023-05-02 10:40:07 -0700

    revert to subflows

--- src/build.py
+++ src/build.py
@@ -8,29 +8,40 @@ def run_sql_build_file(file):
     run_sql_file("sql", file)
     return True
 
+
+@flow
+def build_1():
+    run_sql_build_file("create_priority")
+    run_sql_build_file("create")
+    run_sql_build_file("preprocessing")
+    run_sql_build_file("bbl")
+    
+
+@flow
+def build_2(ready=True):
+    run_sql_build_file("area_zoningdistrict_create")
+    run_sql_build_file("area_commercialoverlay")
+    run_sql_build_file("area_specialdistrict")
+    run_sql_build_file("area_limitedheight")
+    run_sql_build_file("area_zoningmap")
+    return True
+    
+    
+@flow
+def build_3(ready=True):
+    run_sql_build_file("area_zoningdistrict")
+    run_sql_build_file("parks")
+    run_sql_build_file("inzonechange")
+    run_sql_build_file("correct_duplicatevalues")
+    run_sql_build_file("correct_zoninggaps")
+    run_sql_build_file("correct_invalidrecords")
+    return True
+
 @flow
 def build():
-    res_1 = run_sql_build_file.map([
-        "create_priority",
-        "create",
-        "preprocessing",
-        "bbl"
-    ])
-    res_2 = run_sql_build_file.map([
-        "area_zoningdistrict_create",
-        "area_commercialoverlay",
-        "area_specialdistrict",
-        "area_limitedheight",
-        "area_zoningmap"
-    ], wait_for=res_1)
-    res_3 = run_sql_build_file.map([
-        "area_zoningdistrict",
-        "parks",
-        "inzonechange",
-        "correct_duplicatevalues",
-        "correct_zoninggaps",
-        "correct_invalidrecords"
-    ], wait_for=res_2)
+    res_1 = build_1()
+    res_2 = build_2(wait_for=res_1)
+    res_3 = build_3(wait_for=res_2)
     return True
     
     """todo
